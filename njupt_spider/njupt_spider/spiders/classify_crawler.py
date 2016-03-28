f280ed8d41e606afeaf2fc200e96573b6eb739a9
smxxqjl@gmail.com <smxxqjl@gmail.com>
2016-03-28 16:11:22 +0800

    Add status_list to the crawler, and some ugly code orz.

--- njupt_spider/njupt_spider/spiders/classify_crawler.py
+++ njupt_spider/njupt_spider/spiders/classify_crawler.py
@@ -18,20 +18,38 @@ class classifyCrawler(scrapy.Spider):
     def parse(self, response):
         # This is the indexpage parsing, to get the most subdomain of njupt
         depart_list = response.xpath('//*[@id="Map"]/area/@href').extract()
+        title = response.xpath('//title/text()').extract()[0]
+        status_list = list()
+        status_list.append(title)
+        callback = lambda response:self.secondparse(response, status_list)
         for url in depart_list:
-            yield scrapy.Request(url, callback=self.secondparse)
+            yield scrapy.Request(url, callback=callback)
 
-    def secondparse(self, response):
+    def secondparse(self, response, status_list):
         #Search for news block in html generally by guess
         title = response.xpath('//title/text()').extract()[0]
-        print title
-        print response.url
+        if len(status_list) != 1:
+            status_list.pop()
+        status_list.append(title)
         more_list = response.xpath('//div[@align="right"]/a/@href').extract()
+        print '-------------'
+        for string in status_list:
+            print string
+        print '--------------'
+        callback = lambda response:self.thirdparse(response, status_list)
         for url in more_list:
             url = response.urljoin(url)
-            yield scrapy.Request(url, callback=self.thirdparse)
+            yield scrapy.Request(url, callback=callback)
 
-    def thirdparse(self, response):
+    def thirdparse(self, response, status_list):
+
+        title = response.xpath('//title/text()').extract()[0]
+        callback = lambda response:self.thirdparse(response, status_list)
+        for sel in response.xpath('//a'):
+            if len(sel.xpath('@title').extract()) != 0 and "下一页".decode('utf8') in sel.xpath('@title').extract()[0]:
+                nexturl = sel.xpath('@href').extract()[0]
+                nexturl = response.urljoin(nexturl)
+                yield scrapy.Request(nexturl, callback=callback)
 
         for news in response.xpath('//td[@class="postTime"]'):
             item = NjuptSpiderItem()
@@ -39,12 +57,13 @@ class classifyCrawler(scrapy.Spider):
             url = news.xpath('../td/a/@href').extract()[0]
             item['title'] = news.xpath('../td/a/font/text()').extract()[0]
             item['url'] = response.urljoin(url)
-            print item['title']
-            self.fp.write(item['date']+';'+item['title']+';'+item['url']+'\n')
-            yield item
-
-        for sel in response.xpath('//a'):
-            if len(sel.xpath('@title').extract()) != 0 and "下一页".decode('utf8') in sel.xpath('@title').extract()[0]:
-                nexturl = sel.xpath('@href').extract()[0]
-                nexturl = response.urljoin(nexturl)
-                yield scrapy.Request(nexturl, callback=self.thirdparse)
+            print item['title'] + str(len(status_list))
+            outputstring = item['date']+';'+item['title']+';'+item['url']+';'
+            for string in status_list:
+                outputstring = outputstring + string + ';'
+            outputstring = outputstring + title
+            outputstring = outputstring.encode('utf8')
+            #delete all magic line break in string, the world comes in peace
+            outputstring = ''.join(unicode(outputstring, 'utf-8').splitlines())
+            self.fp.write(outputstring+'\n')
+            yield item  
